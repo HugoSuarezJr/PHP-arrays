8648c26b005eb170a8a74567633442b6bc7c41d1
Hugo Suarez <hugo.suarez92@yahoo.com>
2022-02-01 20:38:37 -0500

    array filter

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+class Post {
+    public $title;
+
+    public $published;
+
+    public function __construct($title, $published){
+        $this->title = $title;
+        $this->published = $published;
+
+    }
+}
+
+$posts = [
+    new Post('My First Post', true),
+    new Post('My Second Post', true),
+    new Post('My Third Post', false),
+    new Post('My Fourth Post', true),
+
+];
+
+$unpublishedPosts = array_filter($posts, function ($post){
+    return !$post->published;
+});
+var_dump($unpublishedPosts);
